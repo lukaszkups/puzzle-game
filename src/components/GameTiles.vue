643d9fc559215ec19186c47e80ad4bb29d2fb523
mrmnmly <mrmnmly@gmail.com>
2017-11-10 20:40:22 +0100

    Added game tiles component.

---
+++ src/components/GameTiles.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li 
+      v-for="tile in tiles"
+      :key="tile.order"
+      style="tile.style"
+    ></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      tiles: {
+        type: Array,
+        default: [],
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+
+</style>
