e3bcb65e542ad4abfeb9f63cebc548d0dd07acc5
mrmnmly <mrmnmly@gmail.com>
2017-11-11 00:06:10 +0100

    Updated game code with new components.

--- src/components/GameWrapper.vue
+++ src/components/GameWrapper.vue
@@ -1,49 +1,100 @@
 <template>
   <div class="game-wrapper">
-    <game-tiles
+    <game-grid
       :tiles="tiles"
+    />
+    <puzzle-grid
+      :puzzles="puzzles"
     />
   </div>
 </template>
 
 <script>
-  import GameTiles from './GameTiles';
+  import GameGrid from './GameGrid';
+  import PuzzleGrid from './PuzzleGrid';
 
   import PuzzleImage from './../assets/image.jpg';
-  // 390 in total, 130 per tile
 
   export default {
     data() {
       return {
         image: null,
         tiles: [],
+        puzzles: [],
+        tileSize: 130,
       };
     },
+    computed: {
+      gameStarted() {
+        return this.$store.getters.getStartTime;
+      },
+    },
     methods: {
-      getImageData() {
+      prepareGame() {
         const img = new Image();
         img.onload = () => {
           this.generatePuzzleTiles();
+          while (this.puzzles.length < 3 || this.puzzles.length > 8) {
+            this.shuffleTiles();
+          }
         };
         img.src = PuzzleImage;
       },
       generatePuzzleTiles() {
-        const tileSize = 130;
         for (let x = 0; x < 9; x += 1) {
           this.tiles.push({
             order: x,
             styles: {
-              backgroundPositionX: `-${(x % tileSize) * tileSize}px`,
-              backgroundPositionY: `-${Math.floor(x / tileSize) * tileSize}px`,
-              height: `${tileSize}px`,
-              width: `${tileSize}px`,
+              background: `url(${PuzzleImage}) no-repeat`,
+              backgroundPositionX: `-${(x % 3) * this.tileSize}px`,
+              backgroundPositionY: `-${Math.floor(x / 3) * this.tileSize}px`,
+              height: `${this.tileSize}px`,
+              width: `${this.tileSize}px`,
+              visibility: 'visible',
             },
           });
         }
       },
+      shuffleTiles() {
+        let counter = 1;
+        this.tiles = this.tiles.map((tile) => {
+          const tileClone = tile;
+          const takeTile = Math.random() < 0.5;
+          const imageRandomPosX = Math.floor(Math.random() * 350) + 1;
+          const imageRandomPosY = Math.floor(Math.random() * 175) + 1;
+
+          if (takeTile) {
+            const puzzle = {
+              order: tile.order,
+              styles: {
+                background: `url(${PuzzleImage}) no-repeat`,
+                backgroundPositionX: tile.styles.backgroundPositionX,
+                backgroundPositionY: tile.styles.backgroundPositionY,
+                height: `${this.tileSize}px`,
+                left: `${imageRandomPosX}px`,
+                top: `${imageRandomPosY}px`,
+                width: `${this.tileSize}px`,
+                visibility: 'visible',
+                zIndex: counter,
+              },
+            };
+            counter += 1;
+            this.puzzles.push(puzzle);
+            delete tileClone.styles.background;
+            delete tileClone.styles.backgroundPositionX;
+            delete tileClone.styles.backgroundPositionY;
+            tileClone.styles.visibility = 'hidden';
+          }
+          return tileClone;
+        });
+      },
+    },
+    mounted() {
+      this.prepareGame();
     },
     components: {
-      'game-tiles': GameTiles,
+      'game-grid': GameGrid,
+      'puzzle-grid': PuzzleGrid,
     },
   };
 </script>
