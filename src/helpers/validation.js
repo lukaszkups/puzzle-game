19b3294f808a951a5738fe0e9c27a2bb59f87bb1
mrmnmly <mrmnmly@gmail.com>
2017-11-10 19:03:19 +0100

    Added validation helper module.

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,1 +1,30 @@
-// here will land a mixin of checking if input is valid from InputTextField component file.
+import store from './../store/store';
+
+export const validateLoginForm = (dataToValidate) => {
+  const errors = {
+    blank: "Username can't be blank.",
+    short: 'Username must have at least 3 characters.',
+    long: 'Username is too long (max 20 characters).',
+  };
+  if (!dataToValidate && dataToValidate.length === 0) {
+    store.dispatch('addError', errors.blank);
+    return false;
+  }
+  store.dispatch('removeError', errors.blank);
+  if (dataToValidate.length < 3) {
+    store.dispatch('addError', errors.short);
+    return false;
+  }
+  store.dispatch('removeError', errors.short);
+  if (dataToValidate.length > 20) {
+    store.dispatch('addError', errors.long);
+    return false;
+  }
+  store.dispatch('clearErrors');
+  return true;
+};
+
+// will not be dummy in the future
+export const dummy = () => {
+
+};
