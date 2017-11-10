63987926e7457c5d174a55f2d240d4b19f13f0a6
mrmnmly <mrmnmly@gmail.com>
2017-11-11 00:05:23 +0100

    Updated game tiles code and spread it into separate components.

---
+++ src/components/GameGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="tile-grid">
+    <game-tile
+      v-for="tile in tiles"
+      :key="tile.order"
+      :tileClass="tile-grid__tile"
+      :style="tile.styles"
+      :tile="tile"
+    ></game-tile>
+  </ul>
+</template>
+
+<script>
+  import GameTile from './GameTile';
+
+  export default {
+    props: {
+      tiles: {
+        type: Array,
+        default: [],
+      },
+    },
+    components: {
+      'game-tile': GameTile,
+    },
+  };
+</script>
+
+<style lang="scss">
+  .tile-grid {
+    display: block;
+    height: 396px;
+    list-style-type: none;
+    margin: 0 auto;
+    padding: 0;
+    width: 396px;
+  }
+</style>
---
+++ src/components/GameTile.vue
@@ -0,0 +1,37 @@
+<template>
+  <li
+    class="tile-grid__tile"
+    :style="style"
+    :tile="tile"
+  ></li>
+</template>
+
+<script>
+  export default {
+    props: {
+      style: Object,
+      default: () => {},
+    },
+    tile: {
+      style: Object,
+      default: () => {},
+    },
+    computed: {
+      order() {
+        return this.tile.order;
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+  .tile-grid__tile {
+    border: 1px solid $white;
+    display: block;
+    float: left;
+    height: 130px;
+    margin: 0;
+    padding: 0;
+    width: 130px;
+  }
+</style>
