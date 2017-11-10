2fc78a335fea5a08d42414a128beae41f0094ae0
mrmnmly <mrmnmly@gmail.com>
2017-11-10 20:41:32 +0100

    Removed vue router from the project.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <main-wrapper />
   </div>
 </template>
 
 <script>
+import MainWrapper from './components/MainWrapper';
+
 export default {
   name: 'app',
+  components: {
+    'main-wrapper': MainWrapper,
+  },
 };
 </script>
 
