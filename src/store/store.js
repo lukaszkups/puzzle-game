ff37f07ccf9d5c93c0a7db46afce994800f18a0a
mrmnmly <mrmnmly@gmail.com>
2017-11-12 12:45:59 +0100

    Renamed some stores.

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import popup from './modules/popup';
+import loginPopup from './modules/loginPopup';
 import errors from './modules/errors';
 import timer from './modules/timer';
-import tiles from './modules/tiles';
+import board from './modules/board';
 import puzzle from './modules/puzzle';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    popup,
+    loginPopup,
     errors,
     timer,
-    tiles,
+    board,
     puzzle,
   },
 });
